a39d3be9f6d2080894de8f05932d4121c5af8f7f
ih551311 <ih551311@gmail.com>
2026-03-12 15:57:43 +0900

    260312
    캐릭터움직이기

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string aris = "하이";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("노래방가ㅏ자");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
