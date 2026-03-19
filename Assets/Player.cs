fe725d44feabf2e4cd6c2d62258613d2b4835cf2
ih551311 <ih551311@gmail.com>
2026-03-19 15:47:54 +0900

    플레이어 기본 움직임
    과제

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,16 +4,27 @@ public class Player : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float JumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
 
-    public void OnMove (InputValue value)
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+    public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnJump (InputValue value)
     {
-        
-
+        if (value.isPressed) //점프버튼을 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+        }
+        moveInput = value.Get<Vector2>();
     }
 
     // Update is called once per frame
@@ -27,7 +38,14 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime*moveSpeed);
-        transform.Translate(Vector3.up * moveInput.y * Time.deltaTime*moveSpeed);
     }
 }
